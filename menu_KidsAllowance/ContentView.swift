61aab80ac9d0dde79b408d25c4dd32a9b4d7702d
Ufuoma Okoro <ufookoro@gmail.com>
2019-11-30 18:46:47 +0000

    (1) Add Images to the application

--- menu_KidsAllowance/ContentView.swift
+++ menu_KidsAllowance/ContentView.swift
@@ -12,18 +12,68 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
+    NavigationView {
+        //Spacer().frame(height: 10)
         
-        ZStack(alignment: .trailing) {
-            //Create Rectangle
-            Rectangle()
-                .foregroundColor(.clear)
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
+       
+                //Insert kids pictures
+        VStack {
             
-            //Call MenuItem
-            MenuItem()
+            
+                
+                
+                    //Kinds Picture
+                    VStack {
+                        Spacer().frame(height: 150)
+                        
+                        
+                        Image("davidMugShot")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.gray, lineWidth: 5))
+                        
+                        HStack {
+                        
+                            Image("VictoriaMugShot")
+                                .resizable()
+                                .frame(width: 100, height: 100)
+                                .clipShape(Circle())
+                                .overlay(Circle().stroke(Color.gray, lineWidth: 5))
+                                .scaledToFit()
+                            
+                            Image("bethanyMugShot")
+                                .resizable()
+                                .frame(width: 100, height: 100)
+                                .clipShape(Circle())
+                                .overlay(Circle().stroke(Color.gray, lineWidth: 5))
+                                .scaledToFit()
+                        }
+                    
+                    }
+                    
+                    
         
-        }
+                //Menu
+                ZStack(alignment: .trailing) {
+                        //Create Rectangle
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+                            //Call MenuItem
+                            MenuItem()
         
+                }.padding() //ZStack Ends Here
+            
+            
+            
+    
+    }//Main VStack Ends here
+            
+        .navigationBarTitle(Text("Allowance")) //NavigationView Title
+     
+        } //End of Navigation View
     }
 }
 
