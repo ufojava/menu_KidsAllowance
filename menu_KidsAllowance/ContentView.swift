2b223fdc3408c57b614c623b813bdedd8c8d2bf1
Ufuoma Okoro <ufookoro@gmail.com>
2019-11-30 10:13:05 +0000

    Initial Commit

---
+++ menu_KidsAllowance/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  menu_KidsAllowance
+//
+//  Created by Ufuoma Okoro on 30/11/2019.
+//  Copyright © 2019 Ufuoma Okoro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
